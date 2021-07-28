649ecd30c9f28256b905595895fdc75941d39034
雪猫 <SnowCait@users.noreply.github.com>
2021-07-28 09:32:10 +0900

    Create DB.php

---
+++ db/DB.php
@@ -0,0 +1,22 @@
+<?php
+// select に渡す interface を定義したかったが微妙…
+
+interface RecordInterface;
+
+// テーブル毎の interface だとカラムを絞るのに不便そう
+interface UserRecordInterface implements RecordInterface;
+
+class ActiveRecord
+{
+    public function select(RecordInterface $columns);
+}
+
+class UserRecord implements UserRecordInterface;
+class User
+{
+    public function get(int id): UserRecord
+    {
+        $record = new UserRecord();
+        return $this->db->select($record)->get();
+    }
+}
